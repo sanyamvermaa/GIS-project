66001c83d845ce3ffa67d92fe352af34e7ff84ca
sanyamvermaa <sanyam.verma@iiitb.ac.in>
2025-11-29 01:46:35 +0530

    clipped map

---
+++ Phase5.py
@@ -0,0 +1,24 @@
+import rasterio
+import geopandas as gpd
+from rasterio.mask import mask
+
+# 1. Load the Shapefile ("The Cookie Cutter")
+aoi = gpd.read_file("/Users/sanyam/Desktop/GIS project /IntialData/Intiial dataset/Bengaluru_AOI.geojson")
+
+# 2. Open the Map & Clip It
+with rasterio.open("finalOutput/Final_UHI_Mitigation_Map_Hybrid.tif") as src:
+    # This one line does the actual clipping!
+    out_image, out_transform = mask(src, aoi.geometry, crop=True)
+    out_meta = src.meta.copy()
+
+# 3. Save the Result
+out_meta.update({
+    "height": out_image.shape[1],
+    "width": out_image.shape[2],
+    "transform": out_transform
+})
+
+with rasterio.open("Final_Map_Clipped.tif", "w", **out_meta) as dest:
+    dest.write(out_image)
+
+print("✅ Clipped map saved!")
