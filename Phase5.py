8f7bdfdef9665f831e9f9d2277ade56c25355af0
sanyamvermaa <sanyam.verma@iiitb.ac.in>
2025-11-29 03:43:32 +0530

    clipped map 2

--- Phase5.py
+++ Phase5.py
@@ -6,7 +6,7 @@ from rasterio.mask import mask
 aoi = gpd.read_file("/Users/sanyam/Desktop/GIS project /IntialData/Intiial dataset/Bengaluru_AOI.geojson")
 
 # 2. Open the Map & Clip It
-with rasterio.open("/Users/sanyam/Desktop/GIS project /finalNormalizedData(afterPhase3)/Population_norm.tif") as src:
+with rasterio.open("/Users/sanyam/Desktop/GIS project /finalOutput/Final_UHI_Ensemble_mean.tif") as src:
     # This one line does the actual clipping!
     out_image, out_transform = mask(src, aoi.geometry, crop=True)
     out_meta = src.meta.copy()
@@ -18,7 +18,7 @@ out_meta.update({
     "transform": out_transform
 })
 
-with rasterio.open("Popclipped.tif", "w", **out_meta) as dest:
+with rasterio.open("EnsembLeMeanClipped.tif", "w", **out_meta) as dest:
     dest.write(out_image)
 
 print("✅ Clipped map saved!")
